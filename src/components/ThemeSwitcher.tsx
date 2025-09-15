6ba463543abe4c0c9d8d771e53010b97374ed8d4
Luca Carrozzo <carrozzo.lc@gmail.com>
2025-09-15 09:22:53 +0200

    minor fix

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -7,7 +7,7 @@ const ThemeSwitcher = () => {
   return (
     <div className="flex items-center justify-center gap-3">
       <div>Light</div>
-      <div className="group relative inline-flex w-11 shrink-0 rounded-full bg-gray-200 p-0.5 ring-1 ring-gray-900/5 outline-offset-2 outline-indigo-600 transition-colors duration-200 ease-in-out ring-inset has-[:checked]:bg-indigo-600 has-[:focus-visible]:outline has-[:focus-visible]:outline-2">
+      <div className="group relative inline-flex w-11 shrink-0 rounded-full bg-gray-200 p-0.5 ring-1 ring-gray-900/5 outline-offset-2 outline-indigo-600 transition-colors duration-200 ease-in-out ring-inset has-[:checked]:bg-indigo-600 has-[:focus-visible]:outline">
         <span className="size-5 rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition-transform duration-200 ease-in-out group-has-[:checked]:translate-x-5" />
         <input
           name="setting"
@@ -15,7 +15,7 @@ const ThemeSwitcher = () => {
           aria-label="Use setting"
           checked={theme === 'dark'}
           onChange={toggleTheme}
-          className="absolute inset-0 appearance-none focus:outline-none"
+          className="absolute inset-0 cursor-pointer appearance-none focus:outline-none"
         />
       </div>
       <div>Dark</div>
